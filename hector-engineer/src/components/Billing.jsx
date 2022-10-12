b7473bf137e0fbde7058042d4b8686f539217c99
George Barrett <georgeaustinbarrett@gmail.com>
2022-10-12 15:36:29 +0100

    small styling adjustments to the billng page

--- hector-engineer/src/components/Billing.jsx
+++ hector-engineer/src/components/Billing.jsx
@@ -9,6 +9,15 @@ const Billing = () => (
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient"  />
       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full dimWhite__gradient"  /> 
     </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" /> billing and invoicing</h2>
+      <p>
+        kuefkeuaflkawuhgluglauglaruhgaerhuguuaerhghaelguaugrauhrgareuhglaeugrlaeurglaergaeruglergaegraergeraheahrarerhear
+        kurgaeurglaeruhgluaerguhaerugharegiaeilrugilaeuhuilauhlguarlrgaergkaruhgakuhgkaruehgaeruhglkaeurhglkarugknaergkua
+        krgkaergkareugkaeurugkaerglaeruglkaerugalgralkugalkrugklaugkarugkaerrgleargukaergaerkerkugaergaruegaeugrarlkgurak
+      </p>
+    </div>
   </section>
 )
 
