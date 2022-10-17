0ded97c4c77e0c8bc189546c292f6eaeb39e47d9
George Barrett <georgeaustinbarrett@gmail.com>
2022-10-17 14:26:19 +0100

    testimonial section template complete

--- hector-engineer/src/constants/index.js
+++ hector-engineer/src/constants/index.js
@@ -47,7 +47,7 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+      "Hector is the coolest and most calculated engineer I have worked with.",
     name: "Herman Jensen",
     title: "Founder & Leader",
     img: people01,
@@ -55,7 +55,7 @@ export const feedback = [
   {
     id: "feedback-2",
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+      "There's no other engineer that can deliver on par with Hector.",
     name: "Steve Mark",
     title: "Founder & Leader",
     img: people02,
@@ -63,7 +63,7 @@ export const feedback = [
   {
     id: "feedback-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
+      "The logic and attention to detail left me staggered.",
     name: "Kenn Gallagher",
     title: "Founder & Leader",
     img: people03,
